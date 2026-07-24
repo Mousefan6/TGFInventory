712e02b5e600af675fd6d02b9edbe684c55c0bb4
Bryson-V <97585647+Bryson-V@users.noreply.github.com>
2026-07-24 13:55:34 +1000

    removed lines

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,7 +1,5 @@
 class AppConfig {
   static const String appsheetAppId = 'e4679e1e-7c86-4f09-8575-b14cb0fdc76b';
-
   static const String appsheetAccessKey = 'V2-ff919-SH5QL-CLIkq-hH1jL-rn2N3-vt5PC-Liple-WWzdA';
-
   static const String baseUrl = 'https://api.appsheet.com/api/v2/apps/$appsheetAppId/tables';
 }
